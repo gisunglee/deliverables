6e8dbbf07e36e6f23d117f8040d7e49b34115725
gisunglee <479lgs@naver.com>
2020-06-10 00:20:27 +0900

    여러개 추가

--- src/main/java/silverit/deliverables/controller/ProjectController.java
+++ src/main/java/silverit/deliverables/controller/ProjectController.java
@@ -1,14 +1,59 @@
 package silverit.deliverables.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import silverit.deliverables.common.entity.Project;
+import silverit.deliverables.common.form.ProjectForm;
+import silverit.deliverables.project.repository.ProjectRepository;
+import silverit.deliverables.service.ProjectService;
+import sun.rmi.runtime.Log;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
+@RequiredArgsConstructor
 public class ProjectController {
 
+    final ProjectRepository projectRepository;
+    final ProjectService projectService;
+    
+    
     @RequestMapping("project/list")
-    public String goProjectList(){
+    public String list(Model model){
+        //프로젝트 목록 조회
+        List<Project> projectList = projectRepository.findAll();
+
+        model.addAttribute("projectList", projectList);
 
         return "biz/project/list";
     }
+
+    @RequestMapping("project/goAdd")
+    public String add(ProjectForm projectForm, Model model){
+
+        return "biz/project/add";
+    }
+
+
+    @RequestMapping("project/add")
+    public @ResponseBody ProjectForm save(@ModelAttribute("projectForm") ProjectForm projectForm, Model model){
+
+        Project project = projectService.save(projectForm);
+
+        BeanUtils.copyProperties(project, projectForm);
+
+        System.out.println(projectForm.getPrjNm());
+        System.out.println(projectForm.getPrjNm());
+        System.out.println(projectForm.getPrjNm());
+        System.out.println(projectForm.getPrjNm());
+
+        return projectForm;
+    }
+
 }
--- src/main/java/silverit/deliverables/project/repository/ProjectRepository.java
+++ src/main/java/silverit/deliverables/project/repository/ProjectRepository.java
@@ -1,10 +1,7 @@
 package silverit.deliverables.project.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import silverit.deliverables.common.entity.Output;
 import silverit.deliverables.common.entity.Project;
-import silverit.deliverables.common.entity.ProjectMemberMppg;
 
-public interface ProjectRepository extends JpaRepository<ProjectMemberMppg, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
